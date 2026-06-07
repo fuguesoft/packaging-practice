8a04e74192498c83e85ecadaa8e729361e44bd15
fuguesoft <31870368+fuguesoft@users.noreply.github.com>
2026-06-07 16:51:00 -0500

    Add test file flake template

---
+++ vj.nix
@@ -0,0 +1,27 @@
+# test file from a vimjoyer video
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      packages.${system}.default = pkgs.stdenv.mkDerivation {
+        pname = "";
+        version = "";
+
+        src = "";
+
+        nativeBuildInputs = [
+
+        ];
+        buildInputs = [
+
+        ];
+      };
+    };
+}
