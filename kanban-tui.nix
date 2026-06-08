d18658ec61af3ea3c6869c657356c6f397d88985
fuguesoft <31870368+fuguesoft@users.noreply.github.com>
2026-06-08 18:34:00 -0500

    What is hatching.build and where does it come from?

--- kanban-tui.nix
+++ kanban-tui.nix
@@ -1,30 +1,41 @@
 {
-  # lib,
-  buildPythonPackage,
-  fetchPypi,
-  # python3,
+  lib,
+  python3,
+  fetchFromGitHub,
+  # buildPythonPackage,
+  # setuptools,
   # uv,
   # gnumake,
   # prek,
+  # python314Packages.wheel,
   # writableTmpDirAsHomeHook,
 }:
-buildPythonPackage rec {
+python3.pkgs.buildPythonApplication rec {
   pname = "kanban-tui";
   version = "0.21.1";
+  pyproject = true;
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "FZTUNkoKYJVVn7QD96Z9c9y+kMdAI0eEkZ/jN1I0sbk=";
+  src = fetchFromGitHub {
+    owner = "Zaloog";
+    repo = "kanban-tui";
+    rev = "v${version}";
+    hash = "sha256-FZTUNkoKYJVVn7QD96Z9c9y+kMdAI0eEkZ/jN1I0sbk=";
+    # inherit pname version;
+    # sha256 = "FZTUNkoKYJVVn7QD96Z9c9y+kMdAI0eEkZ/jN1I0sbk=";
     # owner = "Zaloog";
     # repo = "kanban-tui";
     # rev = "v${version}";
     # hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
   };
 
-  nativeBuildInputs = [
+  nativeBuildInputs = with python3.pkgs; [
+    setuptools
     # gnumake
     # writableTmpDirAsHomeHook
   ];
+
+  # build-system = [ setuptools ];
+  # dependencies = [ setuptools ];
 
   buildInputs = [
     # uv
