3a2d8c539c7937afe2ed61d1f3a27ade85403994
fuguesoft <31870368+fuguesoft@users.noreply.github.com>
2026-06-07 21:06:46 -0500

    trying github link instead

--- default.nix
+++ default.nix
@@ -6,7 +6,8 @@ let
   # };
 
   nixpkgs = fetchTarball {
-    url = "https://releases.nixos.org/nixos/26.05/nixos-26.05.1183.6b316287bae2/nixexprs.tar.xz";
+    url = "https://github.com/NixOS/nixpkgs/archive/9b696460ac78b5ccfc17c854d8c976f20456e943.tar.gz";
+    # url = "https://releases.nixos.org/nixos/26.05/nixos-26.05.1183.6b316287bae2/nixexprs.tar.xz";
     sha256 = lib.fakeSha256; # replace after error / with result of nix-prefetch-rul
     # sha256 = "9b696460ac78b5ccfc17c854d8c976f20456e943";
     # sha256 = "0wp4hqw5pgjvmgqq566i064dwg6k97wd1basxc5lwbf2nn2ih8ld";
