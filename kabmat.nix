3c2aff34cd8ea4e598d9faf7efc775d123445fe7
fuguesoft <31870368+fuguesoft@users.noreply.github.com>
2026-06-07 22:58:15 -0500

    will it build?

--- kabmat.nix
+++ kabmat.nix
@@ -4,25 +4,17 @@
   fetchFromGitHub,
   ncurses,
   gnumake,
-  sudo,
-  gcc,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation {
   pname = "kabmat";
   version = "2.7.0";
 
   src = fetchFromGitHub {
     owner = "PlankCipher";
     repo = "kabmat";
-    # tag = "v${version}";
     rev = "02518704976d108a356d685bf351de17142dd76f";
     sha256 = "sha256-AXTSwJTKwN909QgQtVgwDJgYVrI2ED0bJNmbzh/Kph0=";
-    # hash = "02518704976d108a356d685bf351de17142dd76f";
-
-    # sha256 = "02518704976d108a356d685bf351de17142dd76f";
-    # sha256 = "EnkW14L/y20IrSDxO7kbMP33/jEMKCLR6+m3V92BfMQ=";
-
   };
 
   nativeBuildInputs = [
@@ -31,12 +23,11 @@ stdenv.mkDerivation rec {
 
   buildInputs = [
     ncurses
-    gcc
-    sudo
   ];
 
   makeFlags = [
-    "DESTDIR=$(out)"
+    "PREFIX=$(out)"
+    # "DESTDIR=$(out)"
     "DATA_DIR=$(out)"
   ];
 
