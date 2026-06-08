40f0dd8c4d8da92b4e05c4ff3dd5f1048476ddad
fuguesoft <31870368+fuguesoft@users.noreply.github.com>
2026-06-07 21:42:14 -0500

    fixing things...

--- default.nix
+++ default.nix
@@ -7,7 +7,7 @@ let
   nixpkgs = fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/9b696460ac78b5ccfc17c854d8c976f20456e943.tar.gz";
     # url = "https://releases.nixos.org/nixos/26.05/nixos-26.05.1183.6b316287bae2/nixexprs.tar.xz";
-    hash = "sha256-9b696460ac78b5ccfc17c854d8c976f20456e943";
+    sha256 = "sha256-9b696460ac78b5ccfc17c854d8c976f20456e943";
     # sha256 = lib.fakeSha256; # replace after error / with result of nix-prefetch-rul
     # sha256 = "0wp4hqw5pgjvmgqq566i064dwg6k97wd1basxc5lwbf2nn2ih8ld";
     # sha256 = ""; # replace after error / with result of nix-prefetch-rul
