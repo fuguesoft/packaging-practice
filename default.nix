4d847b5d5b5ac3773e1da3bd67580eaf993ba0ae
fuguesoft <31870368+fuguesoft@users.noreply.github.com>
2026-06-07 20:57:11 -0500

    sha256 woes

--- default.nix
+++ default.nix
@@ -1,3 +1,4 @@
+{ lib, ... }:
 let
   # nixpkgs = fetchTarball {
   #   url = "https://github.com/NixOS/nixpkgs/archive/9b696460ac78b5ccfc17c854d8c976f20456e943.tar.gz";
@@ -6,13 +7,13 @@ let
 
   nixpkgs = fetchTarball {
     url = "https://releases.nixos.org/nixos/26.05/nixos-26.05.1183.6b316287bae2/nixexprs.tar.xz";
-    sha256 = "";
-    # sha256 = "0wp4hqw5pgjvmgqq566i064dwg6k97wd1basxc5lwbf2nn2ih8ld"; # replace after error / with result of nix-prefetch-rul
+    sha256 = lib.fakeSha256; # replace after error / with result of nix-prefetch-rul
+    # sha256 = "9b696460ac78b5ccfc17c854d8c976f20456e943";
+    # sha256 = "0wp4hqw5pgjvmgqq566i064dwg6k97wd1basxc5lwbf2nn2ih8ld";
+    # sha256 = ""; # replace after error / with result of nix-prefetch-rul
   };
 
   # nixpkgs = fetchTarball {
-  #   # commit hash for later
-  #   # 9b696460ac78b5ccfc17c854d8c976f20456e943
   #   url = "https://github.com/NixOS/nixpkgs/tarball/nixos-26.05";
   #   sha256 = "";
   # };
