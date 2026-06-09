b3f8e793cf0c723ad48b93ab20a2f448735db0d1
fuguesoft <31870368+fuguesoft@users.noreply.github.com>
2026-06-09 17:13:31 -0500

    Locate uv_build backend

--- textual-jumper.nix
+++ textual-jumper.nix
@@ -1,12 +1,15 @@
 {
   lib,
-  stdenv,
+  # stdenv,
   fetchFromGitHub,
+  python3,
+  python314Packages,
   # gnumake,
 }:
-stdenv.mkDerivation (finalAttrs: {
+python3.pkgs.buildPythonPackage (finalAttrs: {
   pname = "textual-jumper";
   version = "0.2.1";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "Zaloog";
@@ -15,11 +18,14 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-F3TA8ZWAFYxc1vSo2Cu5WSTr78rf/IO7hs+L4voT8Jw=";
   };
 
-  nativeBuildInputs = [
-    # gnumake
+  buildInputs = with python3.pkgs; [
+    uv
   ];
-  buildInputs = [
 
+  nativeBuildInputs = with python3.pkgs; [
+    setuptools
+    wheel
+    hatch
   ];
 
   # makeFlags = [ "PREFIX=$(out)" ];
