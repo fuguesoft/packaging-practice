f7cf31f6ede0bbaf33af5018fbe0dd69bac72112
fuguesoft <31870368+fuguesoft@users.noreply.github.com>
2026-06-13 12:28:46 -0500

    Update kjv package name to verse

--- default.nix
+++ default.nix
@@ -11,7 +11,7 @@ rec {
   textual-jumper = pkgs.python3Packages.callPackage ./pkg_textual-jumper.nix { };
 
   curl-quests = pkgs.callPackage ./pkg_curl-quests.nix { };
-  # kjv = pkgs.callPackage ./pkg_verse.nix { };
+  # verse = pkgs.callPackage ./pkg_verse.nix { };
   # audiowaveform = pkgs.callPackage ./pkg_audiowaveform.nix { };
   flow = pkgs.callPackage ./pkg_flow.nix { };
   crossterm = pkgs.callPackage ./pkg_flow_crossterm.nix { };
